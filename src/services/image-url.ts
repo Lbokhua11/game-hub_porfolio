3dec0326e3e307049c078f194c3be513958735f6
Levyen <89148358+Lbokhua11@users.noreply.github.com>
2024-02-16 14:55:47 +0400

    Show loading skeletons

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,4 +4,4 @@ const index = url.indexOf(target) + target.length;
 return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
 
-export default getCroppedImageUrl
+export default getCroppedImageUrl;
